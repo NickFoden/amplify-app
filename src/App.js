9d37ce78fb721dbaabda92f14974472e305b1dfe
Nick Foden <nickfoden@gmail.com>
2019-05-22 20:24:41 -0400

    Finish sign up with hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import { Auth } from "aws-amplify";
 
+const state = {
+  username: "",
+  password: "",
+  email: ""
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SETFORM":
+      return { ...state, [action.formType]: action.formInput };
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [formState, dispatch] = useReducer(reducer, state);
+
+  async function signUp() {
+    await Auth.signUp({
+      username: formState.username,
+      password: formState.password,
+      attributes: { email: formState.email }
+    });
+  }
+
   useEffect(() => {
     Auth.currentAuthenticatedUser()
       .then(user => console.log({ user }))
@@ -9,9 +34,39 @@ function App() {
   });
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <h1>Hello world</h1>
+      <input
+        placeholder="username"
+        onChange={e =>
+          dispatch({
+            type: "SETFORM",
+            formType: "username",
+            formInput: e.target.value
+          })
+        }
+      />
+      <input
+        placeholder="email"
+        onChange={e =>
+          dispatch({
+            type: "SETFORM",
+            formType: "email",
+            formInput: e.target.value
+          })
+        }
+      />
+      <input
+        placeholder="password"
+        type="password"
+        onChange={e =>
+          dispatch({
+            type: "SETFORM",
+            formType: "password",
+            formInput: e.target.value
+          })
+        }
+      />
+      <button onClick={signUp}>Sign Up</button>
     </div>
   );
 }
